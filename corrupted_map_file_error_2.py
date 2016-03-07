f0d7dec70875884e05af3e053cb15dd62f1ca0cd
Rohmu <riia.luhtala@gmail.com>
2016-03-07 16:37:20 +0200

    Mitenköhän tää mahtaa toimia.

--- corrupted_map_file_error_2.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 7.3.2016
-
-@author: Rohmu
-'''
-class CorruptedMapFileError(Exception):
-
-    def __init__(self, message):
-        super(CorruptedMapFileError, self).__init__(message)
