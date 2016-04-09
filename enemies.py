1e9d580cbe12d8d17d3e4f6073be09b06b5de96f
Luhtala Riia <luhtalr2@niksula.hut.fi>
2016-04-09 13:45:01 +0300

    Created some methods and properties for the enemies class

--- enemies.py
+++ enemies.py
@@ -8,23 +8,66 @@ from PyQt5.QtGui import QPixmap
 
 class Enemy(object):
     def __init__(self, path):
-        self.path = path
-        self.size = 2
-        self.isFinished = False
-        self.isDead = False
+        self._path = path
+        self._size = 2
+        self._isFinished = False
+        self._isDead = False
 
-        self.position_x = self.path[0][0]*20
-        self.position_y = self.path[0][1]*20
+        self._position_x = self.path[0][0]*20
+        self._position_y = self.path[0][1]*20
         
-        self.current_block = self.path[0]
-        self.direction = "RIGHT"
+        self._current_block = self.path[0]
+        self._direction = "RIGHT"
         
 
+    def getPath(self):
+        return self._path
+    
+    
+    def getIsFinished(self):
+        return self._isFinished
+    
+    
+    def setIsFinished(self, boolean):
+        self._isFinished = boolean
+
+    
+    def getIsDead(self):
+        return self._isDead
+    
+    
+    def setIsDead(self, boolean):
+        self._isDead = boolean
+        
+    
+    def getPositionX(self):
+        return self._position_x
+    
+    
+    def getPositionY(self):
+        return self._position_y
+    
+    
+    def getDirection(self):
+        return self._direction
+    
+    
+    def getPicture(self):
+        return self._picture
+
+
+    path = property(getPath)
+    isFinished = property(getIsFinished, setIsFinished)
+    isDead = property(getIsDead, setIsDead)
+    posX = property(getPositionX)
+    posY = property(getPositionY)
+    direction = property(getDirection)   
+    picture = property(getPicture)
 
 
 class Barbarian(Enemy):
     def __init__(self, path):
-        self.name = "Barbarian"
-        self.health = 100
-        self.speed = 3
-        self.picture = QPixmap("barbaari.png")
+        self._name = "Barbarian"
+        self._health = 100
+        self._speed = 3
+        self._picture = QPixmap("barbaari.png")
